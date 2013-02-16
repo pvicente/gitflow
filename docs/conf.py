ab2ef14917a3b87460d5a4c0e0f5a2ae87f85b1c
Hartmut Goebel <h.goebel@crazy-compilers.com>
2013-02-16 16:33:38 +0100

    Exclude boiler-plate files when generating docs.

--- docs/conf.py
+++ docs/conf.py
@@ -64,7 +64,7 @@ release = '0.5.1'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build', 'html', 'man']
+exclude_patterns = ['_build', 'html', 'man', '_subcmd-*.rst', '_manpage-*.rst']
 
 # The reST default role (used for this markup: `text`) to use for all documents.
 #default_role = None
